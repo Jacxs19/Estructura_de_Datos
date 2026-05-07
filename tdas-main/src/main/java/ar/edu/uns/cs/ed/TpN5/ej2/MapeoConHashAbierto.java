48c8467ec31a1c4dd69cb966ab0c1da6f3bd57fd
Jacxs19 <tomasfholowaty@hotmail.com>
2026-05-07 20:02:30 -0300

    Ahora entiendo maaas, tp5

--- tdas-main/src/main/java/ar/edu/uns/cs/ed/TpN5/ej2/MapeoConHashAbierto.java
+++ tdas-main/src/main/java/ar/edu/uns/cs/ed/TpN5/ej2/MapeoConHashAbierto.java
@@ -1,17 +1,21 @@
 package ar.edu.uns.cs.ed.TpN5.ej2;
+import ar.edu.uns.cs.ed.tdas.excepciones.InvalidKeyException;
+import ar.edu.uns.cs.ed.tdas.tdalista.*;
+import ar.edu.uns.cs.ed.tdas.Entry;
+
 import ar.edu.uns.cs.ed.TpN4.Ej1.ListaDoblementeEnlazada;
 import ar.edu.uns.cs.ed.tdas.tdamapeo.Map;
 
 public class MapeoConHashAbierto<K,V>implements Map<K,V>{
-    private  Map<K,V>[] A;
+    private  PositionList<Entry<K,V>>[] A;
     int n;                              //Cantidad de entradas en el mapeo
     int N=17;               
     
     //Constructor
     public MapeoConHashAbierto(){
-        A= (Map<K,V>[]) new MapeoConLista[N];
+        A= new ListaDoblementeEnlazada[N];
         for(int i=0; i<N-1; i++)
-            A[i]= new MapeoConLista<K,V>();
+            A[i]= new ListaDoblementeEnlazada<Entry<K,V>>();
     }
     public int size(){
         return n;
@@ -19,5 +23,49 @@ public class MapeoConHashAbierto<K,V>implements Map<K,V>{
     public boolean isEmpty(){
         return n==0;
     }
+    public V get (K key){
+        if(key==null)
+            throw new InvalidKeyException("Llave invalida");
+
+        PositionList<Entry<K,V>> bucket = A[h(key)];
+        
+        for(Entry<K,V> entrada : bucket){
+            if(entrada.getKey().equals(key)){
+                return entrada.getValue();
+            }
+        }
+
+        return null;
+    }
+    public V put (K key, V value){
+        if(key==null)
+            throw new InvalidKeyException("Llave invalida");
+        PositionList<Entry<K,V>> bucket= A[h(key)];
+        for(Entry<K,V> aux : bucket){
+            if(aux.getKey().equals(key)){
+                V valorViejo = aux.getValue();
+                aux.setValue(value);
+                return valorViejo;
+            }
+        }
+        Entry<K,V> entrada = new Entrada<K,V>(key, value);
+        bucket.addLast(entrada);
+        n++;
+
+    }
+
+        
+
     
+}
+    
+    
+
+
+
+
+
+    private int h(K key){
+        return key.hashCode() % N;
+    }
 }
